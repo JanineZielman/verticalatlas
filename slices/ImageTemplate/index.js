4fd20129a7d2447121cc08e691e3d062e0e60566
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-10-18 11:56:32 +0200

    cool update

--- slices/ImageTemplate/index.js
+++ slices/ImageTemplate/index.js
@@ -3,26 +3,28 @@ import { PrismicRichText } from '@prismicio/react'
 
 const ImageTemplate = ({ slice }) => (
   <section className='image-template'>
-    {slice.items.map((item, i) => {
-      const size = item.image_size
-      return(
-        <div key={`image-template-item-${i}`} className="content-wrapper image-item">
-          {item.image.url &&
-            <img src={item.image[size]?.url ? item.image[size].url : item.image.url}/>
-          }
-          {item.image_caption[0] &&
-            <div className='caption'>
-              <PrismicRichText field={item.image_caption}/>
-            </div>
-          }
-        </div>
-      )
-    })}
     {slice.primary.text[0] &&
       <div className='text-wrapper'>
         <PrismicRichText field={slice.primary.text}/>
       </div>
     }
+    <div className='images'>
+      {slice.items.map((item, i) => {
+        const size = item.image_size
+        return(
+          <div key={`image-template-item-${i}`} className="content-wrapper image-item">
+            {item.image.url &&
+              <img src={item.image[size]?.url ? item.image[size].url : item.image.url}/>
+            }
+            {item.image_caption[0] &&
+              <div className='caption'>
+                <PrismicRichText field={item.image_caption}/>
+              </div>
+            }
+          </div>
+        )
+      })}
+    </div>
   </section>
 )
 
