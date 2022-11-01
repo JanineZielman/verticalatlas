40e207da3388ff7062f222a04f5c3ec2b07c5bef
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-11-01 16:06:26 +0100

    update

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,20 @@ export default function Document() {
           rel="stylesheet"
         />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer" async/>
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-0S4K85MJXW"
+          />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0S4K85MJXW', { 'anonymize_ip': true });
+            `,
+          }}
+        />
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
